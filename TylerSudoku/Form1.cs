f0806b9b4e39f1e7f19388417869e150fe51a8f7
Tyler Hsia <thsia@overlake.org>
2020-06-08 21:59:57 -0700

    text box object, select all, inputs only numbers, tab fixes

--- TylerSudoku/Form1.cs
+++ TylerSudoku/Form1.cs
@@ -15,6 +15,20 @@ namespace TylerSudoku
         public Form1()
         {
             InitializeComponent();
+            for(var i = 0; i < 9; i++)
+            {
+                for(var j = 0; j < 9; j++)
+                {
+                    var newCell = new MyCellBox(this, i, j);
+                    newCell.Width = 30;
+                    newCell.Height = 30;
+                    newCell.Left = 100 + j * 33;
+                    newCell.Top = 100 + i * 33;
+                    this.Controls.Add(newCell);
+                    
+                }
+            }
+            
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -35,5 +49,71 @@ namespace TylerSudoku
 
 
         
+    }
+
+
+    public class MyCellBox : TextBox
+    {
+        private Form1 form1;
+        private int x;
+        private int y;
+
+        public MyCellBox(Form1 form1, int i, int j)
+        {
+            this.form1 = form1;
+            this.x = i;
+            this.y = j;
+            this.MaxLength = 1;
+        }
+
+        protected override void OnKeyUp(KeyEventArgs e)
+        {
+            
+            base.OnKeyUp(e);
+            if(e.KeyCode > Keys.D0 && e.KeyCode < Keys.D9)
+            {
+                if (this.Text.Length > 0 && isDigit(this.Text))
+                {
+                    if (x == 8 && y == 8)
+                    {
+                        var array = form1.Controls.OfType<MyCellBox>().ToArray();
+                        array[0].Select();
+                        this.SelectAll();
+                    }
+                    else
+                    {
+                        form1.GetNextControl(this, true).Select();
+                        this.SelectAll();
+                    }
+                }
+            }
+            if (!isDigit(this.Text) && this.Text.Length > 0)
+            {
+                this.BackColor = Color.Red;
+                this.Text = "";
+            }
+            if(isDigit(this.Text) || this.Text.Length == 0)
+            {
+                this.BackColor = Color.White;
+            }
+        }
+
+        public bool isDigit(String input)
+        {
+            if(input.Length == 1)
+            {
+                if(input.Equals("1") || input.Equals("2") || input.Equals("3") || input.Equals("4") || input.Equals("5") || input.Equals("6") || input.Equals("7") || input.Equals("8") || input.Equals("9"))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        protected override void OnClick(EventArgs e)
+        {
+            base.OnClick(e);
+            this.SelectAll();
+        }
     }
 }
